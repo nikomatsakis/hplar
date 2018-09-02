d64fe4176c03285a77b35d584d8748022c1afd36
Niko Matsakis <niko@alum.mit.edu>
2018-09-02 06:51:06 -0400

    Initial Commit

---
+++ hplar/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hplar
+//
+//  Created by Nicholas Matsakis on 9/2/18.
+//  Copyright © 2018 Nicholas Matsakis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
